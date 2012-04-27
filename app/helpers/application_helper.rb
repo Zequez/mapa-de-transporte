a2d98a279367a14c9d5b75be812b046288ab180d
Zequez <zequez@gmail.com>
2012-04-27 03:47:02 -0700

    Added meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,28 @@ module ApplicationHelper
   def show_help_tips?
     cookies[:help_tips] != 'false'
   end
+
+  def title
+    @page_title ||= if @bus
+      t('views.head.title_bus', bus: @bus.name, city: @city.name)
+    elsif @city
+      t('views.head.title_city', city: @city.name)
+    else
+      t('views.head.title')
+    end
+  end
+
+  def description
+    @page_description ||= if @bus
+      t('views.head.description_bus', bus: @bus.name, city: @city.name)
+    elsif @city
+      t('views.head.description_city', city: @city.name)
+    else
+      t('views.head.description')
+    end
+  end
+
+  def url
+    
+  end
 end
